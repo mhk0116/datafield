6d78b775e43cefc4c1ff944f06a6d97b8f8f7966
mhk0116 <mhk0116@gmail.com>
2020-06-06 23:06:55 +0900

    Change trend page

--- src/routes/Trend.js
+++ src/routes/Trend.js
@@ -42,14 +42,31 @@ class Trend extends React.Component {
     }
   render() {
     const {list1, rentalNo, imgSrc, imageHash} = this.state;
+    const getMyList = () =>{
+      const myList = [];
+      for(let i = 3; i < list1.length; i++){
+        if((i+1)%4 == 0){
+          let temp = {"name":list1[i], "id":list1[i+1], "address":list1[i+2], "mean":list1[i+3],}
+          myList.push(temp);
+        }
+      }
+      return myList;
+    }
+    const myList = list1.slice(3);
     return (
       <div>
-        <h3>{list1.map((d)=>{
-          console.log(d);
-        })}</h3>
         <input type ="text" placeholder="대여소 번호 입력 후 엔터" value={rentalNo} onChange={this.handleChange } onKeyPress = {this.handleKeyPress} />
         <img src={`${imgSrc}?${imageHash}`} />
         <Link to="/datafield">Datafield</Link>
+        <div>
+          <h1>{list1[0]}</h1>
+          <h1>{list1[1]}</h1>
+          <h1>{list1[2]}</h1>
+          <div>{getMyList().map((d)=>{
+            const temp = d.name + "\t\t" + d.id + "\t\t" +  d.address + "\t\t" + d.mean
+            return (<p>{temp}</p>)
+          })}</div>
+        </div>
         <Comment id="1" page="trend" />
       </div>
     );
