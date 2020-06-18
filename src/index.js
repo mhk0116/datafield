11c1a0131cd8c711fa4be0b733787846dcc7905c
mhk0116 <mhk0116@gmail.com>
2020-06-18 19:27:42 +0900

    Update hamberger and cluster css because of resolution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import 'react-app-polyfill/ie11';
+// import 'react-app-polyfill/stable';
 import {HashRouter} from "react-router-dom";
 import "./index.css";
 import App from "./App";
